b9d9af1305537e35fd92502ae769b52c7fdcab24
Andrey Postovoiy <113462667+paseidon72@users.noreply.github.com>
2023-01-12 12:08:43 +0200

    videoreder

--- Shifrovanie/deshifr.py
+++ Shifrovanie/deshifr.py
@@ -2,23 +2,22 @@ import pyAesCrypt
 import os
 import sys
 
-
-# функция шифрования файла
-def encryption(file, password):
+# функция дешифровки файла
+def decryption(file, password):
 
     # задаём размер буфера
     buffer_size = 512 * 1024
 
-    # вызываем метод шифрования
-    pyAesCrypt.encryptFile(
+    # вызываем метод расшифровки
+    pyAesCrypt.decryptFile(
         str(file),
-        str(file) + ".crp",
+        str(os.path.splitext(file)[0]),
         password,
         buffer_size
     )
 
     # чтобы видеть результат выводим на печать имя зашифрованного файла
-    print("[Файл '" + str(os.path.splitext(file)[0]) + "' зашифрован]")
+    print("[Файл '" + str(os.path.splitext(file)[0]) + "' дешифрован]")
 
     # удаляем исходный файл
     os.remove(file)
@@ -30,10 +29,10 @@ def walking_by_dirs(dir, password):
     for name in os.listdir(dir):
         path = os.path.join(dir, name)
 
-        # если находим файл, то шифруем его
+        # если находим файл, то дешифруем его
         if os.path.isfile(path):
             try:
-                encryption(path, password)
+                decryption(path, password)
             except Exception as ex:
                 print(ex)
         # если находим директорию, то повторяем цикл в поисках файлов
@@ -41,6 +40,6 @@ def walking_by_dirs(dir, password):
             walking_by_dirs(path, password)
 
 
-password = input("Введите пароль для шифрования: ")
+password = input("Введите пароль для расшифровки: ")
 walking_by_dirs("путь к файлу", password)
 # os.remove(str(sys.argv[0]))
